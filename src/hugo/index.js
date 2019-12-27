5df4b4a29ca51e4f013598f33fc6586fc94ecabf
Henrique Dias <hacdias@gmail.com>
2019-12-27 19:50:24 +0000

    limit

--- src/hugo/index.js
+++ src/hugo/index.js
@@ -3,9 +3,10 @@ const path = require('path')
 const yaml = require('js-yaml')
 const slugify = require('@sindresorhus/slugify')
 const { spawnSync } = require('child_process')
-
+const pLimit = require('p-limit')
 module.exports = class HugoManager {
   constructor ({ dir }) {
+    this.limit = pLimit(1)
     this.dir = dir
     this.contentDir = path.join(dir, 'content')
   }
@@ -22,81 +23,83 @@ module.exports = class HugoManager {
   }
 
   newPost ({ properties, commands }) {
-    const date = new Date()
-    const year = date.getFullYear().toString()
-    const month = (date.getMonth() + 1).toString().padStart(2, '0')
-    const day = date.getDate().toString().padStart(2, '0')
+    return this.limit(() => {
+      const date = new Date()
+      const year = date.getFullYear().toString()
+      const month = (date.getMonth() + 1).toString().padStart(2, '0')
+      const day = date.getDate().toString().padStart(2, '0')
 
-    const content = properties.content
-      ? properties.content.join('\n').trim()
-      : ''
-
-    delete properties.content
-
-    const meta = {
-      title: properties.name
-        ? properties.name.join(' ').trim()
-        : '',
-      description: null,
-      date,
-      categories: [],
-      tags: [],
-      aliases: [],
-      properties: {}
-    }
-
-    delete properties.name
-
-    if (meta.title === '' && content === '') {
-      throw new Error('must have title or content')
-    }
-
-    if (meta.title === '') {
-      meta.title = content.length > 15
-        ? content.substring(0, 15) + '...'
-        : content
-    }
-
-    // TODO: correctly parse location
-    // and get more info
-
-    if (properties.category) meta.tags = properties.category
-
-    let slug = commands['mp-slug']
-      ? commands['mp-slug'][0]
-      : meta.title
-        ? slugify(meta.title)
+      const content = properties.content
+        ? properties.content.join('\n').trim()
         : ''
 
-    if (properties['bookmark-of']) {
-      meta.categories = ['bookmarks']
-      slug = ''
-    } else {
-      meta.categories = ['notes']
-    }
+      delete properties.content
 
-    meta.properties = properties
+      const meta = {
+        title: properties.name
+          ? properties.name.join(' ').trim()
+          : '',
+        description: null,
+        date,
+        categories: [],
+        tags: [],
+        aliases: [],
+        properties: {}
+      }
 
-    const num = this._getNextPostNumber(year, month, day)
-    const alias = `/${year}/${month}/${day}/${num}/`
-    const url = `${alias}${slug}`
+      delete properties.name
 
-    if (slug !== '') {
-      meta.aliases = [alias]
-    }
+      if (meta.title === '' && content === '') {
+        throw new Error('must have title or content')
+      }
 
-    const dirPath = path.join(this.contentDir, url)
-    const indexPath = path.join(dirPath, 'index.md')
-    const index = `---\n${yaml.safeDump(meta, { sortKeys: true })}---\n\n${content}`
+      if (meta.title === '') {
+        meta.title = content.length > 15
+          ? content.substring(0, 15) + '...'
+          : content
+      }
 
-    fs.ensureDirSync(dirPath, { recursive: true })
-    fs.writeFileSync(indexPath, index)
+      // TODO: correctly parse location
+      // and get more info
 
-    let res = spawnSync('git', ['add', '-A'], { cwd: this.dir })
-    if (res.error) throw res.error
-    res = spawnSync('git', ['commit', '-m', `add ${dirPath}`], { cwd: this.dir })
-    if (res.error) throw res.error
+      if (properties.category) meta.tags = properties.category
 
-    return `https://hacdias.com${url}`
+      let slug = commands['mp-slug']
+        ? commands['mp-slug'][0]
+        : meta.title
+          ? slugify(meta.title)
+          : ''
+
+      if (properties['bookmark-of']) {
+        meta.categories = ['bookmarks']
+        slug = ''
+      } else {
+        meta.categories = ['notes']
+      }
+
+      meta.properties = properties
+
+      const num = this._getNextPostNumber(year, month, day)
+      const alias = `/${year}/${month}/${day}/${num}/`
+      const url = `${alias}${slug}`
+
+      if (slug !== '') {
+        meta.aliases = [alias]
+      }
+
+      const dirPath = path.join(this.contentDir, url)
+      const indexPath = path.join(dirPath, 'index.md')
+      const index = `---\n${yaml.safeDump(meta, { sortKeys: true })}---\n\n${content}`
+
+      fs.ensureDirSync(dirPath, { recursive: true })
+      fs.writeFileSync(indexPath, index)
+
+      let res = spawnSync('git', ['add', '-A'], { cwd: this.dir })
+      if (res.error) throw res.error
+      res = spawnSync('git', ['commit', '-m', `add ${dirPath}`], { cwd: this.dir })
+      if (res.error) throw res.error
+
+      return `https://hacdias.com${url}`
+    })
   }
 }
