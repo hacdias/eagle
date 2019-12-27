801c6fc62d23ff79fcb19e8f14f1d905c30820a3
Henrique Dias <hacdias@gmail.com>
2019-12-27 22:11:25 +0000

    fix 404

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 const HugoManager = require('./hugo')
 
 const micropub = require('./routes/micropub')
@@ -17,6 +17,6 @@ const hugo = new HugoManager({
 app.use('/micropub', micropub({ hugo }))
 app.use('/webmention', webmention({ hugo }))
 app.get('/robots.txt', robots)
-app.get(r404)
+app.use(r404)
 
 app.listen(port, () => console.log(`Listening on port ${port}!`))
