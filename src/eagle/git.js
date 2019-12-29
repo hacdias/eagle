f3627f50ac2b2a03ccb47f556ec68ecb7d616fe8
Henrique Dias <hacdias@gmail.com>
2019-12-29 23:49:28 +0000

    print stderr

--- src/eagle/git.js
+++ src/eagle/git.js
@@ -2,8 +2,12 @@ const { spawnSync } = require('child_process')
 
 const commit = (message, opts) => {
   let res = spawnSync('git', ['add', '-A'], opts)
+  let stderr = res.stderr.toString()
+  console.log(stderr)
   if (res.error) throw res.error
   res = spawnSync('git', ['commit', '-m', message], opts)
+  stderr = res.stderr.toString()
+  console.log(stderr)
   if (res.error) throw res.error
 }
 
