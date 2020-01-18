88599b7d311015f821354d657440ebd7eafd603a
Henrique Dias <hacdias@gmail.com>
2020-01-18 20:18:46 +0000

    remove wrong script

--- bin/missing-tweets.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config()
-
-const got = require('got')
-const eagle = require('../src/eagle').fromEnvironment()
-
-;(async () => {
-  const res = await eagle.twitter.like('1211725899047034881')
-  console.log(res)
-})()
