3c817ae1f340608a13a3e6ebd663694def905302
Henrique Dias <hacdias@gmail.com>
2020-01-18 20:58:34 +0000

    cleanups scripts

--- bin/fetch-webmentions.js
+++ bin/fetch-webmentions.js
@@ -1,3 +1,6 @@
+#!/usr/bin/env node
+'use strict'
+
 require('dotenv').config()
 
 const got = require('got')
--- bin/xray-missing.js
+++ bin/xray-missing.js
@@ -1,3 +1,6 @@
+#!/usr/bin/env node
+'use strict'
+
 require('dotenv').config()
 
 const { join } = require('path')
