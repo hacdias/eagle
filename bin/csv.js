84ebca3a5ed3078b93a73180b4f0d8802d7736c3
Henrique Dias <hacdias@gmail.com>
2020-02-02 23:59:54 +0000

    produce some csvs

---
+++ bin/csv.js
@@ -0,0 +1,78 @@
+#!/usr/bin/env node
+'use strict'
+
+require('dotenv').config()
+
+const csv = require('@fast-csv/format')
+const fs = require('fs-extra')
+const config = require('../src/config')()
+const hugo = require('../src/services/hugo')(config.hugo)
+
+async function outputBookmarks () {
+  const stream = csv.format({
+    headers: [
+      'Title',
+      'Date Added',
+      'URL',
+      'Tags'
+    ]
+  })
+  stream.pipe(fs.createWriteStream('bookmarks.csv'))
+
+  for await (const { meta } of hugo.getAll()) {
+    if (!meta.properties) {
+      continue
+    }
+
+    if (!meta.categories || !meta.categories.includes('bookmarks')) {
+      continue
+    }
+
+    stream.write([meta.title, meta.date.getTime(), meta.properties['bookmark-of'], meta.tags])
+  }
+
+  stream.end()
+}
+
+async function outputReads () {
+  const stream = csv.format({
+    headers: [
+      'Status',
+      'Date',
+      'Author',
+      'Name',
+      'Rating',
+      'UID',
+      'Tags'
+    ]
+  })
+  stream.pipe(fs.createWriteStream('books.csv'))
+
+  for await (const { meta } of hugo.getAll()) {
+    if (!meta.properties) {
+      continue
+    }
+
+    if (!meta.categories || !meta.categories.includes('reads')) {
+      continue
+    }
+
+    stream.write([
+      meta.properties['read-status'],
+      meta.date.getTime(),
+      meta.properties['read-of'][0].properties.author,
+      meta.properties['read-of'][0].properties.name,
+      meta.properties['read-of'][0].properties.rating || 0,
+      meta.properties['read-of'][0].properties.uid,
+      meta.properties['bookmark-of'],
+      meta.tags
+    ])
+  }
+
+  stream.end()
+}
+
+;(async () => {
+  await outputBookmarks()
+  await outputReads()
+})()
