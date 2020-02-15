9602f09544fb4715195e5ff06b6edfcdea629243
Henrique Dias <hacdias@gmail.com>
2020-02-15 16:50:41 +0000

    remove newline

--- src/services/bot.js
+++ src/services/bot.js
@@ -5,13 +5,13 @@ module.exports = function createTelegram ({ token, chatID, git, hugo }) {
     echo: ({ reply }) => reply('echo'),
     push: async ({ reply }) => {
       const { stdout } = await git.push()
-      reply(`Pushed!\n\n\`\`\`\n${stdout}\n\`\`\``, {
+      reply(`Pushed!\n\`\`\`\n${stdout}\n\`\`\``, {
         parse_mode: 'Markdown'
       })
     },
     pull: async ({ reply }) => {
       const { stdout } = await git.pull()
-      reply(`Pulled!\n\n\`\`\`\n${stdout}\n\`\`\``, {
+      reply(`Pulled!\n\`\`\`\n${stdout}\n\`\`\``, {
         parse_mode: 'Markdown'
       })
     },
