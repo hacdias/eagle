8140ce649caecd9b29e438e59a23c7699131f878
Henrique Dias <hacdias@gmail.com>
2020-02-23 20:30:17 +0000

    hook

--- src/server/hook-notes.js
+++ src/server/hook-notes.js
@@ -4,7 +4,10 @@ const crypto = require('crypto')
 
 module.exports = ({ git, buildKB, secret }) => ar(async (req, res) => {
   console.log(req.body)
-  const sig = 'sha1=' + crypto.createHmac('sha1', secret).update(req.body.toString()).digest('hex')
+  const sig = 'sha1=' + crypto
+    .createHmac('sha1', secret)
+    .update(JSON.stringify(req.body))
+    .digest('hex')
 
   if (req.headers['x-hub-signature'] !== sig) {
     return res.sendStatus(403)
