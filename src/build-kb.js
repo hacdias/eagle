1a8db3fc7b9e02a5015445d038a71ea7a6e84adf
Henrique Dias <hacdias@gmail.com>
2020-02-24 10:02:19 +0000

    fix build kb

--- src/build-kb.js
+++ src/build-kb.js
@@ -3,51 +3,49 @@ const { join } = require('path')
 const yaml = require('js-yaml')
 const slugify = require('slugify')
 
-module.exports = function buildKB ({ src, dst }) {
-  return async () => {
-    await fs.remove(dst)
-    await fs.ensureDir(dst)
+module.exports = async function buildKB ({ src, dst }) {
+  await fs.remove(dst)
+  await fs.ensureDir(dst)
 
-    await fs.outputFile(
-      join(dst, '_index.md'),
+  await fs.outputFile(
+    join(dst, '_index.md'),
     `---
 title: Knowledge Base
 emoji: 🧠
 ---`
-    )
+  )
 
-    const files = await fs.readdir(src)
+  const files = await fs.readdir(src)
 
-    for (const index of files) {
-      const path = join(src, index)
-      if (fs.statSync(path).isDirectory()) {
-        continue
-      }
-
-      const file = (await fs.readFile(path)).toString()
-      let [frontmatter, content] = file.split('\n---')
-      const meta = yaml.safeLoad(frontmatter)
-
-      if ((meta.tags && meta.tags.includes('private')) || meta.deleted) {
-        continue
-      }
-
-      meta.date = meta.modified
-      meta.publishDate = meta.created
-
-      delete meta.modified
-      delete meta.created
-      delete meta.pinned
-
-      content = content.trim()
-      if (content[0] === '#') {
-        content = content.substring(content.indexOf('\n')).trim()
-      }
-
-      await fs.outputFile(
-        join(dst, `${slugify(meta.title.toLowerCase())}.md`),
-      `---\n${yaml.safeDump(meta, { sortKeys: true })}---\n\n${content.trim()}`
-      )
+  for (const index of files) {
+    const path = join(src, index)
+    if (fs.statSync(path).isDirectory()) {
+      continue
     }
+
+    const file = (await fs.readFile(path)).toString()
+    let [frontmatter, content] = file.split('\n---')
+    const meta = yaml.safeLoad(frontmatter)
+
+    if ((meta.tags && meta.tags.includes('private')) || meta.deleted) {
+      continue
+    }
+
+    meta.date = meta.modified
+    meta.publishDate = meta.created
+
+    delete meta.modified
+    delete meta.created
+    delete meta.pinned
+
+    content = content.trim()
+    if (content[0] === '#') {
+      content = content.substring(content.indexOf('\n')).trim()
+    }
+
+    await fs.outputFile(
+      join(dst, `${slugify(meta.title.toLowerCase())}.md`),
+      `---\n${yaml.safeDump(meta, { sortKeys: true })}---\n\n${content.trim()}`
+    )
   }
 }
