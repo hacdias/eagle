bdb8868f2fe1c57fbf14507c9e1cfca181e36ad3
Henrique Dias <hacdias@gmail.com>
2020-10-07 19:09:12 +0200

    start syndication

--- services/gossip.go
+++ services/gossip.go
@@ -2,60 +2,60 @@ package services
 
 import (
 	"bytes"
+	"log"
+	"net/url"
 
 	"github.com/PuerkitoBio/goquery"
 )
+
+func (s *Services) getWebmentionTargets(entry *HugoEntry) ([]string, error) {
+	html, err := s.Hugo.GetEntryHTML(entry.ID)
+	if err != nil {
+		return nil, err
+	}
+
+	doc, err := goquery.NewDocumentFromReader(bytes.NewReader(html))
+	if err != nil {
+		return nil, err
+	}
+
+	targets := []string{}
+
+	doc.Find(".h-entry .e-content p").Each(func(i int, q *goquery.Selection) {
+		val, ok := q.Attr("href")
+		if !ok {
+			return
+		}
+
+		u, err := url.Parse(val)
+		if err != nil {
+			// ???
+			return
+		}
+
+		if u.Host == "" {
+			u.Host = s.cfg.Domain
+		}
+
+		targets = append(targets, u.String())
+	})
+
+	return targets, err
+}
 
 // Gossip takes care of the interactions of a certain post with the world.
 func (s *Services) Gossip(entry *HugoEntry, syn *Syndication) {
 	url := s.cfg.Domain + entry.ID
 
-	html, err := s.Hugo.GetEntryHTML(entry.ID)
+	targets, err := s.getWebmentionTargets(entry)
 	if err != nil {
-		return
+		log.Printf("failed to get webmentions targets: %s", err)
+	} else {
+		err = s.Webmentions.Send(url, targets...)
+		if err != nil {
+			log.Printf("failed to send webmentions: %s", err)
+		}
 	}
 
-	goquery.NewDocumentFromReader(bytes.NewReader(html))
-
+	// TODO: syndicate to twitter
 }
-
-/*
-
-const getMentions = async (url, body) => {
-  debug('will scrap %s for webmentions', url)
-  const parsed = parse(body)
-
-  const targets = parsed.querySelectorAll('.h-entry .e-content a')
-    .map(p => p.attributes.href)
-    .map(href => {
-      try {
-        const u = new URL(href, url)
-        return u.href
-      } catch (_) {
-        return href
-      }
-    })
-
-  debug('found webmentions: %o', targets)
-  return targets
-}
-
-const sendWebmentions = async (post, url, related, services) => {
-  const { hugo, notify, webmentions } = services
-  const targets = [...related]
-
-  try {
-    const html = await hugo.getEntryHTML(post)
-    const mentions = await getMentions(url, html)
-    targets.push(...mentions)
-  } catch (err) {
-    notify.sendError(err)
-  }
-
-  try {
-    await webmentions.send({ source: url, targets })
-  } catch (err) {
-    notify.sendError(err)
-  }
-}
-*/
