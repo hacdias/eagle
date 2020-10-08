1f240d3fe636f67b1fda11f60a8e6232bd193729
Henrique Dias <hacdias@gmail.com>
2020-10-08 13:33:46 +0200

    feat: accept mf2 and as2
    License: MIT
    Signed-off-by: Henrique Dias <hacdias@gmail.com>

--- server/static.go
+++ server/static.go
@@ -1,9 +1,12 @@
 package server
 
 import (
+	"io"
 	"net/http"
 	"path"
 	"strings"
+
+	"willnorris.com/go/microformats"
 )
 
 type notFoundRedirectRespWr struct {
@@ -26,18 +29,65 @@ func (w *notFoundRedirectRespWr) Write(p []byte) (int, error) {
 }
 
 func staticHandler(dir string) func(http.ResponseWriter, *http.Request) {
-	fs := http.FileServer(http.Dir(dir))
+	httpdir := http.Dir(dir)
+	fs := http.FileServer(httpdir)
+
+	findActivity := func(url string) string {
+		filepath := path.Join(url, "index.as2")
+		// TODO: I'd prefer just stat and not open. Is it better like this
+		// or serve it directly?
+		if fd, err := httpdir.Open(filepath); err == nil {
+			fd.Close()
+			return filepath
+		}
+
+		return ""
+	}
+
+	getHTML := func(url string) io.ReadCloser {
+		if !strings.HasSuffix(url, ".html") {
+			url = path.Join(url, "index.html")
+		}
+
+		fd, err := httpdir.Open(url)
+		if err != nil {
+			return nil
+		}
+
+		return fd
+	}
 
 	return func(w http.ResponseWriter, r *http.Request) {
 		accept := r.Header.Get("Accept")
 		acceptsHTML := strings.Contains(accept, "text/html")
 		acceptsActivity := strings.Contains(accept, "application/activity+json") || strings.Contains(accept, `application/ld+json; profile="https://www.w3.org/ns/activitystreams"`)
+		acceptsMf2 := strings.Contains(accept, "application/mf2+json")
 
-		if acceptsActivity && !acceptsHTML {
-			// TODO(activitypub): set content-type and show .as2 instead if exists
+		if strings.HasSuffix(r.URL.Path, ".as2") {
+			w.Header().Set("Content-Type", "application/activity+json; charset=utf-8")
 		}
 
-		// TODO: set Content-Type on .as2
+		if !acceptsHTML {
+			if acceptsActivity {
+				if as2 := findActivity(r.URL.Path); as2 != "" {
+					w.Header().Set("Content-Type", "application/activity+json; charset=utf-8")
+					r.URL.Path = as2
+				}
+			}
+
+			if acceptsMf2 {
+				// TODO: simplify and add alternate meta on HTML
+				fd := getHTML(r.URL.Path)
+				if fd != nil {
+					defer fd.Close()
+					data := microformats.Parse(fd, r.URL)
+					if data != nil {
+						serveJSON(w, http.StatusOK, data)
+						return
+					}
+				}
+			}
+		}
 
 		nfw := &notFoundRedirectRespWr{ResponseWriter: w}
 		fs.ServeHTTP(nfw, r)
