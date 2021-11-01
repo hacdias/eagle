925fee8cde4e2ba2cad73ba59f8ae4b992bc1cce
Henrique Dias <hacdias@gmail.com>
2021-11-01 18:35:01 +0100

    v2: bring back micropub request parser
    License: MIT
    Signed-off-by: Henrique Dias <hacdias@gmail.com>

---
+++ pkg/micropub/discovery.go
@@ -0,0 +1,98 @@
+package micropub
+
+import (
+	"regexp"
+	"strings"
+
+	"github.com/karlseguin/typed"
+)
+
+// Type represents a post type.
+type Type string
+
+const (
+	TypeRsvp     Type = "rsvp"
+	TypeRepost   Type = "repost"
+	TypeLike     Type = "like"
+	TypeReply    Type = "reply"
+	TypeBookmark Type = "bookmark"
+	TypeFollow   Type = "follow"
+	TypeRead     Type = "read"
+	TypeWatch    Type = "watch"
+	TypeCheckin  Type = "checkin"
+	TypeVideo    Type = "video"
+	TypeAudio    Type = "audio"
+	TypePhoto    Type = "photo"
+	TypeEvent    Type = "event"
+	TypeRecipe   Type = "recipe"
+	TypeReview   Type = "review"
+	TypeNote     Type = "note"
+	TypeArticle  Type = "article"
+)
+
+var propertyToType = map[string]Type{
+	"rsvp":        TypeRsvp,
+	"repost-of":   TypeRepost,
+	"like-of":     TypeLike,
+	"in-reply-to": TypeReply,
+	"bookmark-of": TypeBookmark,
+	"follow-of":   TypeFollow,
+	"read-of":     TypeRead,
+	"watch-of":    TypeWatch,
+	"checkin":     TypeCheckin,
+	"video":       TypeVideo,
+	"audio":       TypeAudio,
+	"photo":       TypePhoto,
+}
+
+// DiscoverType discovers a post type from its properties according to the algorithm
+// described in the "Post Type Discovery" specification.
+// 	- https://www.w3.org/TR/post-type-discovery/
+// 	- https://indieweb.org/post-type-discovery
+//
+// This is a slightly modified version of @aaronpk's code to include reads and watches.
+// Original code: https://github.com/aaronpk/XRay/blob/master/lib/XRay/PostType.php
+func DiscoverType(properties typed.Typed) Type {
+	if typ, ok := properties.StringIf("type"); ok {
+		switch typ {
+		case "event", "recipe", "review":
+			return Type(typ)
+		}
+	}
+
+	for key, val := range propertyToType {
+		if _, ok := properties[key]; ok {
+			return val
+		}
+	}
+
+	nameSlice, exists := properties.StringsIf("name")
+	if !exists {
+		return TypeNote
+	}
+
+	name := strings.TrimSpace(strings.Join(nameSlice, ""))
+	if name == "" {
+		return TypeNote
+	}
+
+	content := ""
+	if val, ok := properties.MapIf("content"); ok {
+		content = val["text"].(string)
+	} else if val, ok := properties.StringIf("summary"); ok {
+		content = val
+	}
+
+	// Collapse all sequences of internal whitespace to a single space (0x20) character each
+	var re = regexp.MustCompile(`/\s+/`)
+	name = re.ReplaceAllString(name, " ")
+	content = re.ReplaceAllString(content, " ")
+
+	// If this processed "name" property value is NOT a prefix of the
+	// processed "content" property, then it is an article post.
+	if strings.Index(content, name) != 0 {
+		return TypeArticle
+	}
+
+	return TypeNote
+}
