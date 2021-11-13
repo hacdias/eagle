0ba69baa40241c8decf1e26957a0d4a2c1cfed24
Henrique Dias <hacdias@gmail.com>
2021-11-13 13:14:38 +0100

    v2: update assets

--- eagle/eagle.go
+++ eagle/eagle.go
@@ -4,12 +4,10 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
-	"strings"
 	"sync"
 	"time"
 
 	"github.com/hacdias/eagle/v2/config"
-	"github.com/hacdias/eagle/v2/contenttype"
 	"github.com/hacdias/eagle/v2/entry"
 	"github.com/hacdias/eagle/v2/entry/mf2"
 	"github.com/hacdias/eagle/v2/fs"
@@ -19,11 +17,6 @@ import (
 	"github.com/jackc/pgx/v4/pgxpool"
 	"github.com/spf13/afero"
 	"github.com/tdewolff/minify/v2"
-	mCss "github.com/tdewolff/minify/v2/css"
-	mHtml "github.com/tdewolff/minify/v2/html"
-	mJs "github.com/tdewolff/minify/v2/js"
-	mJson "github.com/tdewolff/minify/v2/json"
-	mXml "github.com/tdewolff/minify/v2/xml"
 
 	"github.com/yuin/goldmark"
 	"go.uber.org/zap"
@@ -47,9 +40,12 @@ type Eagle struct {
 	syndication  *syndicator.Manager
 	allowedTypes []mf2.Type
 
-	assets *Assets
+	// This can be changed while in development mode.
+	assets    *Assets
+	templates map[string]*template.Template
 
-	templates        map[string]*template.Template
+	// Things that are initialized once.
+	redirects        map[string]string
 	markdown         goldmark.Markdown
 	absoluteMarkdown goldmark.Markdown
 	minifier         *minify.M
@@ -94,7 +90,7 @@ func NewEagle(conf *config.Config) (*Eagle, error) {
 		allowedTypes: []mf2.Type{},
 		syndication:  syndicator.NewManager(),
 		Parser:       entry.NewParser(conf.Site.BaseURL),
-		minifier:     getMinifier(),
+		minifier:     initMinifier(),
 	}
 
 	for typ := range conf.Site.MicropubTypes {
@@ -136,14 +132,23 @@ func NewEagle(conf *config.Config) (*Eagle, error) {
 		e.Miniflux = &Miniflux{Miniflux: conf.Miniflux}
 	}
 
-	err = e.ReloadAssets()
+	err = e.initRedirects()
 	if err != nil {
 		return nil, err
 	}
 
-	err = e.updateTemplates()
+	err = e.initAssets()
 	if err != nil {
 		return nil, err
+	}
+
+	err = e.initTemplates()
+	if err != nil {
+		return nil, err
+	}
+
+	if e.Config.Development {
+		go e.reloader()
 	}
 
 	return e, nil
@@ -151,32 +156,6 @@ func NewEagle(conf *config.Config) (*Eagle, error) {
 
 func (e *Eagle) GetSyndicators() []*syndicator.Config {
 	return e.syndication.Config()
-}
-
-func (e *Eagle) GetRedirects() (map[string]string, error) {
-	redirects := map[string]string{}
-
-	data, err := e.fs.ReadFile("redirects")
-	if err != nil {
-		return nil, err
-	}
-
-	strs := strings.Split(string(data), "\n")
-
-	for _, str := range strs {
-		if strings.TrimSpace(str) == "" {
-			continue
-		}
-
-		parts := strings.Split(str, " ")
-		if len(parts) != 2 {
-			e.log.Warnf("found invalid redirect entry: %s", str)
-		}
-
-		redirects[parts[0]] = parts[1]
-	}
-
-	return redirects, nil
 }
 
 func (e *Eagle) Close() {
@@ -189,15 +168,20 @@ func (e *Eagle) userAgent(comment string) string {
 	return fmt.Sprintf("Eagle/0.0 %s", comment)
 }
 
-func getMinifier() *minify.M {
-	m := minify.New()
-	m.AddFunc(contenttype.HTML, mHtml.Minify)
-	m.AddFunc(contenttype.CSS, mCss.Minify)
-	m.AddFunc(contenttype.XML, mXml.Minify)
-	m.AddFunc(contenttype.JS, mJs.Minify)
-	m.AddFunc(contenttype.RSS, mXml.Minify)
-	m.AddFunc(contenttype.ATOM, mXml.Minify)
-	m.AddFunc(contenttype.JSONFeed, mJson.Minify)
-	m.AddFunc(contenttype.AS, mJson.Minify)
-	return m
+// TODO(v2): improve this template/assets reloader
+// for development mode. Fsnotify?
+func (e *Eagle) reloader() {
+	for {
+		time.Sleep(time.Second * 10)
+
+		err := e.initAssets()
+		if err != nil {
+			e.log.Error(err)
+		}
+
+		err = e.initTemplates()
+		if err != nil {
+			e.log.Error(err)
+		}
+	}
 }
