bbd8916ac92c164eea351fd6cc0df00376cd3cf4
Henrique Dias <hacdias@gmail.com>
2022-10-29 19:46:27 +0100

    feat: play with time slug

--- entry/utils.go
+++ entry/utils.go
@@ -21,13 +21,21 @@ func NewSlug() string {
 	return string(b)
 }
 
+func NewTimeSlug(t time.Time) string {
+	ns := t.Nanosecond()
+	for ns > 99 {
+		ns /= 10
+	}
+	return fmt.Sprintf("%02dh%02dm%02ds%02d", t.Hour(), t.Minute(), t.Second(), ns)
+}
+
 func NewID(slug string, t time.Time) string {
 	if t.IsZero() {
 		t = time.Now().Local()
 	}
 
 	if slug == "" {
-		slug = NewSlug()
+		slug = NewTimeSlug(t)
 	}
 
 	return fmt.Sprintf("/%04d/%02d/%02d/%s", t.Year(), t.Month(), t.Day(), slug)
