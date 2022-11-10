05a8d01e329234cc53e5c240fab0e4f309f8578d
Henrique Dias <hacdias@gmail.com>
2022-11-10 17:21:56 +0100

    always same order

--- server/server.go
+++ server/server.go
@@ -14,6 +14,7 @@ import (
 	"path"
 	"path/filepath"
 	"runtime/debug"
+	"sort"
 	"strconv"
 	"strings"
 	"sync"
@@ -330,6 +331,7 @@ func (s *Server) getActions() []string {
 	for action := range s.actions {
 		actions = append(actions, action)
 	}
+	sort.Strings(actions)
 	return actions
 }
 
