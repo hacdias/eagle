42a98e891c87542cfa7d407ac8b5778e4ac2c4f1
Henrique Dias <hacdias@gmail.com>
2022-11-15 21:58:08 +0100

    feat: sanitize html from xray

--- pkg/xray/utils.go
+++ pkg/xray/utils.go
@@ -3,13 +3,17 @@ package xray
 import (
 	"regexp"
 	"strings"
+
+	"github.com/microcosm-cc/bluemonday"
 )
 
 var (
+	htmlRemover    = bluemonday.StrictPolicy()
 	spaceCollapser = regexp.MustCompile(`\s+`)
 )
 
 func SanitizeContent(data string) string {
+	data = htmlRemover.Sanitize(data)
 	data = strings.TrimSpace(data)
 	data = spaceCollapser.ReplaceAllString(data, " ") // Collapse whitespaces
 	return data
