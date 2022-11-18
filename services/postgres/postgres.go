6066bb36b00f4303553076693929d8c03bb969cb
Henrique Dias <hacdias@gmail.com>
2022-11-18 13:12:01 +0100

    Revert "fix: truncate postgres table on startup"
    This reverts commit 71bba45f099c28b2b4bbbb4d4cec2e5c809911e5.

--- services/postgres/postgres.go
+++ services/postgres/postgres.go
@@ -36,9 +36,6 @@ func NewPostgres(cfg *eagle.PostgreSQL) (*Postgres, error) {
 		return nil, err
 	}
 
-	// Truncate table to keep records clean.
-	_, _ = pool.Exec(context.Background(), "truncate entries cascade")
-
 	return &Postgres{pool}, nil
 }
 
