b35ea5d6dc76d8a5f5a1834d19d74e74b1f0e3a2
Henrique Dias <hacdias@gmail.com>
2022-12-29 10:09:58 +0000

    feat: clean redirects cmd

---
+++ cmd/eagle/clean-redirects.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/hacdias/eagle/eagle"
+	"github.com/hacdias/eagle/fs"
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	rootCmd.AddCommand(cleanRedirectsCmd)
+}
+
+var cleanRedirectsCmd = &cobra.Command{
+	Use: "clean-redirects",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		c, err := eagle.ParseConfig()
+		if err != nil {
+			return err
+		}
+
+		fs := fs.NewFS(c.Source.Directory, c.Server.BaseURL, &fs.NopSync{})
+
+		redirects, err := fs.LoadRedirects(false)
+		if err != nil {
+			return err
+		}
+
+		var resolveRedirect func(u string) string
+		resolveRedirect = func(u string) string {
+			if r, ok := redirects[u]; ok {
+				return resolveRedirect(r)
+			}
+
+			return u
+		}
+
+		for src := range redirects {
+			redirects[src] = resolveRedirect(src)
+
+			_, err := fs.GetEntry(redirects[src])
+			if os.IsNotExist(err) {
+				delete(redirects, src)
+			}
+		}
+
+		for src, dst := range redirects {
+			fmt.Printf("%s %s\n", src, dst)
+		}
+
+		return nil
+	},
+}
