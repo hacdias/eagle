0006b6a00b9155791211b0e67d912461a8a1202d
Henrique Dias <hacdias@gmail.com>
2023-01-05 20:20:42 +0100

    feat: add 'count' command

---
+++ cmd/eagle/count.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+
+	"github.com/hacdias/eagle/eagle"
+	"github.com/hacdias/eagle/fs"
+	"github.com/samber/lo"
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	rootCmd.AddCommand(countCmd)
+}
+
+var countCmd = &cobra.Command{
+	Use: "count",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		c, err := eagle.ParseConfig()
+		if err != nil {
+			return err
+		}
+
+		fs := fs.NewFS(c.Source.Directory, c.Server.BaseURL, &fs.NopSync{})
+		ee, err := fs.GetEntries(false)
+		if err != nil {
+			return err
+		}
+
+		count := map[string]int{}
+
+		for _, e := range ee {
+			for _, section := range e.Sections {
+				if _, ok := count[section]; !ok {
+					count[section] = 0
+				}
+
+				count[section]++
+			}
+		}
+
+		keys := lo.Keys(count)
+		sort.Strings(keys)
+
+		for _, k := range keys {
+			fmt.Printf("%s: %d\n", k, count[k])
+		}
+
+		return nil
+	},
+}
