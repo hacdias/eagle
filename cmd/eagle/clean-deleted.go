1357dc5eee7a43697dfef192294a7d5d252fdbe2
Henrique Dias <hacdias@gmail.com>
2023-02-25 15:35:42 +0100

    feat: clean-deleted command

---
+++ cmd/eagle/clean-deleted.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"path/filepath"
+
+	"github.com/hacdias/eagle/eagle"
+	eaglefs "github.com/hacdias/eagle/fs"
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	rootCmd.AddCommand(cleanDeletedCmd)
+}
+
+var cleanDeletedCmd = &cobra.Command{
+	Use: "clean-deleted",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		c, err := eagle.ParseConfig()
+		if err != nil {
+			return err
+		}
+
+		fs := eaglefs.NewFS(c.Source.Directory, c.Server.BaseURL, &eaglefs.NopSync{})
+		ee, err := fs.GetEntries(false)
+		if err != nil {
+			return err
+		}
+
+		for _, e := range ee {
+			if e.Deleted {
+				err = fs.RemoveAll(filepath.Join(eaglefs.ContentDirectory, e.ID))
+				if err != nil {
+					return err
+				}
+			}
+		}
+
+		return nil
+	},
+}
