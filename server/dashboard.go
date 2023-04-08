20cb17966bb8f0ff8d02723f6a0f880cc392c4aa
Henrique Dias <hacdias@gmail.com>
2023-04-08 20:55:51 +0200

    feat: media upload on dashboard

--- server/dashboard.go
+++ server/dashboard.go
@@ -1,9 +1,14 @@
 package server
 
 import (
+	"bytes"
 	"fmt"
+	"io"
 	"net/http"
+	"path/filepath"
+	"strings"
 
+	"github.com/gabriel-vasile/mimetype"
 	"github.com/hacdias/eagle/eagle"
 	"github.com/hacdias/eagle/renderer"
 	"github.com/hacdias/indieauth/v3"
@@ -11,9 +16,10 @@ import (
 )
 
 type dashboardData struct {
-	Actions []string
-	Success bool
-	Token   string
+	Actions       []string
+	Success       bool
+	Token         string
+	MediaLocation string
 }
 
 func (s *Server) dashboardGet(w http.ResponseWriter, r *http.Request) {
@@ -29,8 +35,24 @@ func (s *Server) dashboardPost(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	if r.Form.Get("action") != "" {
+		s.dashboardPostAction(w, r)
+		return
+	} else if r.Form.Get("token") == "true" {
+		s.dashboardPostToken(w, r)
+		return
+	} else if err := r.ParseMultipartForm(20 << 20); err == nil {
+		s.dashboardPostUpload(w, r)
+		return
+	} else {
+		fmt.Println(err)
+	}
+
+	s.dashboardGet(w, r)
+}
+
+func (s *Server) dashboardPostAction(w http.ResponseWriter, r *http.Request) {
 	actions := r.Form["action"]
-	token := r.Form.Get("token") == "true"
 	data := &dashboardData{
 		Actions: s.getActions(),
 	}
@@ -47,29 +69,89 @@ func (s *Server) dashboardPost(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	if token {
-		clientID := r.Form.Get("client_id")
-		scope := r.Form.Get("scope")
-		expiry, err := handleExpiry(r.Form.Get("expiry"))
-		if err != nil {
-			s.serveErrorHTML(w, r, http.StatusBadRequest, fmt.Errorf("expiry param is invalid: %w", err))
-		}
+	s.serveDashboard(w, r, data)
+}
 
-		if err := indieauth.IsValidClientIdentifier(clientID); err != nil {
-			s.serveErrorHTML(w, r, http.StatusBadRequest, fmt.Errorf("invalid client_id: %w", err))
-			return
-		}
+func (s *Server) dashboardPostToken(w http.ResponseWriter, r *http.Request) {
+	data := &dashboardData{
+		Actions: s.getActions(),
+	}
 
-		signed, err := s.generateToken(clientID, scope, expiry)
-		if err == nil {
-			data.Token = signed
-		} else {
-			s.serveErrorHTML(w, r, http.StatusInternalServerError, err)
-			return
-		}
+	clientID := r.Form.Get("client_id")
+	scope := r.Form.Get("scope")
+	expiry, err := handleExpiry(r.Form.Get("expiry"))
+	if err != nil {
+		s.serveErrorHTML(w, r, http.StatusBadRequest, fmt.Errorf("expiry param is invalid: %w", err))
+	}
+
+	if err := indieauth.IsValidClientIdentifier(clientID); err != nil {
+		s.serveErrorHTML(w, r, http.StatusBadRequest, fmt.Errorf("invalid client_id: %w", err))
+		return
+	}
+
+	signed, err := s.generateToken(clientID, scope, expiry)
+	if err == nil {
+		data.Token = signed
+	} else {
+		s.serveErrorHTML(w, r, http.StatusInternalServerError, err)
+		return
 	}
 
 	s.serveDashboard(w, r, data)
+}
+
+func (s *Server) dashboardPostUpload(w http.ResponseWriter, r *http.Request) {
+	file, header, err := r.FormFile("file")
+	if err != nil {
+		s.serveErrorHTML(w, r, http.StatusBadRequest, err)
+		return
+	}
+	defer file.Close()
+
+	raw, err := io.ReadAll(file)
+	if err != nil {
+		s.serveErrorHTML(w, r, http.StatusBadRequest, err)
+		return
+	}
+
+	fmt.Println(header.Filename)
+
+	ext := filepath.Ext(header.Filename)
+	if ext == "" {
+		// NOTE: I'm not using http.DetectContentType because it depends
+		// on OS specific mime type registries. Thus, it was being unreliable
+		// on different OSes.
+		contentType := header.Header.Get("Content-Type")
+		mime := mimetype.Lookup(contentType)
+		if mime.Is("application/octet-stream") {
+			mime = mimetype.Detect(raw)
+		}
+
+		if mime == nil {
+			s.serveErrorHTML(w, r, http.StatusBadRequest, err)
+			return
+		}
+
+		ext = mime.Extension()
+	}
+
+	var location string
+
+	if r.Form.Get("preserve-filename") == "on" {
+		location, err = s.media.UploadMedia(strings.TrimSuffix(header.Filename, ext), ext, bytes.NewReader(raw))
+	} else {
+		location, err = s.media.UploadAnonymousMedia(ext, bytes.NewReader(raw))
+	}
+
+	if err != nil {
+		s.serveErrorHTML(w, r, http.StatusInternalServerError, err)
+		return
+	}
+
+	s.serveDashboard(w, r, &dashboardData{
+		Actions:       s.getActions(),
+		MediaLocation: location,
+	})
 }
 
 func (s *Server) serveDashboard(w http.ResponseWriter, r *http.Request, data *dashboardData) {
