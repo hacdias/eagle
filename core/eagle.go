b3aa45e3f1956bb6953f82b3d7d15da984545dff
Henrique Dias <hacdias@gmail.com>
2023-11-18 23:01:31 +0100

    core: correct generation of permalinks

--- core/eagle.go
+++ core/eagle.go
@@ -62,7 +62,6 @@ type Entries []*Entry
 type FrontMatter struct {
 	Title       string         `yaml:"title,omitempty"`
 	Description string         `yaml:"description,omitempty"`
-	URL         string         `yaml:"url,omitempty"`
 	Draft       bool           `yaml:"draft,omitempty"`
 	Date        time.Time      `yaml:"date,omitempty"`
 	Lastmod     time.Time      `yaml:"lastmod,omitempty"`
